e392e7ad074df79ccd9a8a63a015fe806b7f4e83
Mike Dunn <mgdunnvt@gmail.com>
2020-01-19 19:32:59 -0500

    add an page for /wisdom

--- duval-static/src/pages/wisdom/index.js
+++ duval-static/src/pages/wisdom/index.js
@@ -1,6 +1,6 @@
 import React from "react"
-import Container from "../components/container"
-import Header from "../components/header"
+import Container from "../../components/container"
+import Header from "../../components/header"
 import { Link, graphql } from "gatsby"
 
 export default ({ data }) => {
